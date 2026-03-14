95bf282b04ff628b09ec6d67310826145e24e0dc
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2026-03-14 15:35:56 +0500

    Technical Preview

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -17,46 +16,8 @@ public class OptionsManager : MonoBehaviour
 
     public bool isChanging = false;
 
-    public void OnDropdownChanged(int index)
-    {
-        if (isChanging) return;
-        StartCoroutine(SetLocale(index));
-    }
-
-    IEnumerator SetLocale(int index)
-    {
-        isChanging = true;
-
-        yield return LocalizationSettings.InitializationOperation;
-
-        switch (index)
-        {
-            case 0:
-                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.GetLocale("en");
-                break;
-            case 1:
-                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.GetLocale("ru");
-                break;
-        }
-
-        isChanging = false;
-    }
     void Start()
     {
-        var currentLocale = LocalizationSettings.SelectedLocale.Identifier.Code;
-
-        switch (currentLocale)
-        {
-            case "en":
-                dropdown.value = 0;
-                break;
-            case "ru":
-                dropdown.value = 1;
-                break;
-            default:
-                dropdown.value = 0;
-                break;
-        }
         if (PlayerPrefs.HasKey("MouseSent"))
         {
             float mouseSent = PlayerPrefs.GetFloat("MouseSent");
