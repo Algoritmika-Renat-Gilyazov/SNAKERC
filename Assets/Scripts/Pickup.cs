703b9bde103f678b6ad7a69706445521f9d137ab
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2026-04-10 13:52:43 +0500

    Pickup Animation

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,5 +4,32 @@ public class Pickup : MonoBehaviour
 {
     public Item item; // Имя или тип предмета
 
-    // Можно добавить дополнительные параметры (например, иконку, количество и т.д.)
+    public float height = 1.5f;
+    public float duration = 1f;
+    
+    private Vector3 posA, posB;
+    private float timer = 0f;
+    private bool toB = true;
+    
+    void Start()
+    {
+        posA = transform.position;
+        posB = posA + Vector3.up * height;
+    }
+    
+    void Update()
+    {
+        timer += Time.deltaTime / duration;
+        
+        if (toB)
+            transform.position = Vector3.Lerp(posA, posB, timer);
+        else
+            transform.position = Vector3.Lerp(posB, posA, timer);
+        
+        if (timer >= 1f)
+        {
+            timer = 0f;
+            toB = !toB;
+        }
+    }
 }
